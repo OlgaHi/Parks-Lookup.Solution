9fc6b41c98226b60bdcf0271443f9e7e376d86e1
Olha Hizhytska <olgainfotech@gmail.com>
2021-04-02 16:16:54 -0700

    add seeding database

--- ParksSearch/Models/ParksSearchContext.cs
+++ ParksSearch/Models/ParksSearchContext.cs
@@ -11,5 +11,22 @@ namespace ParksSearch.Models
 
         public DbSet<NationalPark> NationalParks { get; set; }
         public DbSet<StatePark> StateParks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<NationalPark>()
+                .HasData(
+                    new NationalPark { Id = 1, Name = "Olympic Park", Address = "3002 Mt Angeles Rd, Port Angeles, WA", WheelchairAccess = true },
+                    new NationalPark { Id = 2, Name = "Mountain Rainier Park", Address = "55210 238th Avenue East, Ashford, WA", WheelchairAccess = false },
+                    new NationalPark { Id = 3, Name = "Paradise", Address = "3256 Road, Paradise, WA", WheelchairAccess = true }
+                    );
+
+            builder.Entity<StatePark>()
+                .HasData(
+                    new StatePark { Id = 1, Name = "Bridle Trails", Address = "5300 116th Ave NE, Kirkland, WA", WheelchairAccess = true },
+                    new StatePark { Id = 1, Name = "Saint Edward", Address = "555 NE, Kenmore, WA", WheelchairAccess = true },
+                    new StatePark { Id = 1, Name = "Kitsap Memorial", Address = "1111 NE 136 St, Poulsbo, WA", WheelchairAccess = true }
+                    );
+        }
     }
 }
